4852dbfa2ee9a21b3ce5397440f829d81c8bf5b4
Jonathan Carey <jonathan.carey1995@gmail.com>
2024-09-13 17:12:42 -0400

    added health getter and setter

--- Base/base.gd
+++ Base/base.gd
@@ -1,12 +1,22 @@
 extends Node3D
 
 @export var max_health: int = 5
+
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		print("health was changed")
+		base_health.text = str(current_health)
+		if current_health < 1:
+			get_tree().reload_current_scene()
+		
 @onready var base_health: Label3D = $Label3D
 
+
+
 func _ready() -> void:
-	base_health.text = str(max_health)
+	current_health = max_health
 
 func take_damage() -> void:
 	print("Damage dealt to base!")
-	max_health -= 1
-	base_health.text = str(max_health)
+	current_health -= 1
